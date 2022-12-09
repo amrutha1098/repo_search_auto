d9b9f7feb481b67e3b810de2ef70b67c8deb6d88
Amrutha <amruthar1098@gmail.com>
2022-12-09 11:33:06 +0530

    added remaining ui operations for repo details modal + fetching the drop down details

--- scripts/common_util/ui_helper/browser_helper.py
+++ scripts/common_util/ui_helper/browser_helper.py
@@ -80,6 +80,25 @@ class BROWSER_HELPER:
         time.sleep(5)
         # replace with wait for page to load ..
 
+    def select_close_tab(self):
+        button_xpath = "//button[contains(@class,'MuiButtonBase-root MuiIconButton-root MuiIconButton-sizeMedium css-8g0vn4-MuiButtonBase-root-MuiIconButton-root')]"
+
+        button = self.driver.find_element(By.XPATH, value=button_xpath)
+        button.click()
+
+        time.sleep(5)
+        # replace with wait for page to load .. 
+    
+    def select_accept_tab(self):
+        button_xpath = "//button[contains(@class,'MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeMedium MuiButton-textSizeMedium MuiButtonBase-root  css-1q7oytv-MuiButtonBase-root-MuiButton-root')]"
+
+        button = self.driver.find_element(By.XPATH, value=button_xpath)
+        button.click()
+
+        time.sleep(5)
+        # replace with wait for page to load ..  
+
+
     """
     Code to fetch values from ui
     """
@@ -129,12 +148,42 @@ class BROWSER_HELPER:
         print(table_data)
         return table_data
 
-    def fetch_page_details(self):
+    def fetch_number_page_details(self):
         xpath = "//p[contains(@class, 'MuiTablePagination-displayedRows css-11ceysh-MuiTablePagination-displayedRows')]"
         element = self.driver.find_element(By.XPATH, xpath)
         # print(element.text)
         return element.text
 
+    def fetch_repo_details(self, identifier):
+        details_path = "//div[contains(@class, 'MuiGrid-root css-vj1n65-MuiGrid-root')]"
+        identity_path = "/following::*[(text()='" + str(identifier) +"')]/../following-sibling::p"
+        xpath = details_path + identity_path
+        # print(xpath)
+        details = self.driver.find_element(By.XPATH, xpath)
+        return details.text
+
+    def fetch_commit_fork_details(self, value):
+        
+        self.select_get_details(value)
+        
+        data = {}
+        data["commit_details"] = self.fetch_repo_details(identifier = "Last 3 committers: ")
+        data["fork_details"] = self.fetch_repo_details(identifier="Recent Forked User: ")
+        data["fork_bio_details"] = self.fetch_repo_details(identifier="Recent Forked User Bio: ")
+        # print(data)
+
+        self.select_accept_tab()
+
+        return data
+
+    def fetch_drop_down_details(self):
+        drop_down_xpath = "//*[text()='" + str(
+            "Rows per page:") + "']" + "/following::*[contains(@class, 'MuiTablePagination-select MuiSelect-select MuiSelect-standard MuiInputBase-input css-194a1fa-MuiSelect-select-MuiInputBase-input')]"
+        drop_down = self.driver.find_element(By.XPATH, value=drop_down_xpath)
+
+        # print(drop_down.text)
+        return drop_down.text
+        
     """
     does page has certain elements
     """
--- scripts/test_cases/repo_seach.py
+++ scripts/test_cases/repo_seach.py
@@ -19,15 +19,17 @@ class SimplisticTest(unittest.TestCase):
         ui_obj = BROWSER_HELPER()
         ui_obj.invoker_browser()
         ui_obj.get_url("http://localhost:3000/")
-        # ui_obj.search_text("test")
-        # ui_obj.select_drop_down(25)
+        ui_obj.search_text("test")
+        ui_obj.select_drop_down(25)
         # ui_obj.select_next_prev_button("next")
         # ui_obj.select_next_prev_button("previous")
         # ui_obj.select_get_details(9)
         # ui_obj.fetch_name_from_table(7)
         # ui_obj.fetch_table_headers()
-        # ui_obj.does_initial_text()
-        ui_obj.fetch_page_details()
+        # ui_obj.fetch_initial_text()
+        # ui_obj.fetch_number_page_details()
+        # ui_obj.fetch_commit_fork_details(1)
+        ui_obj.fetch_drop_down_details()
 
 
 if __name__ == '__main__':
